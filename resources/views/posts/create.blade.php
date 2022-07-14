b6cef2b2d14c5a911b28ca864c1cbada818b3d06
DalitsoKasonde <dkasonde11@gmail.com>
2022-07-14 16:26:08 +0200

    blog posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,13 +7,13 @@
 <form action="{{ route('posts.store') }}" method="POST">
     @csrf
     <div>
-        <input type="text" name="title" >
+        <input type="text" name="title" value="{{ old('title') }}">
     </div>
     @error('title')
         <div>{{ $message }}</div>
     @enderror
     <div>
-        <textarea name="content"></textarea>
+        <textarea name="content">{{ old('content') }}</textarea>
     </div>
     @error('content')
     <div>{{ $message }}</div>
